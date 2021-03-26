d910044ef0ad74843168b64949ce3b9e7a78cee7
wtfychxx <fadhliyulyanto@gmail.com>
2021-03-26 23:06:57 +0700

    add a new page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'true-gray': colors.trueGraym,
+        'cyan': colors.cyan
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
+    container: {
+      center: true
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      justifyContent: ['hover', 'focus']
+    },
   },
   plugins: [],
 }
