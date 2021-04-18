9abb694f9ecf6473c6aa0cac9c4ba163c441fc3f
wtfychxx <fadhliyulyanto@gmail.com>
2021-04-18 13:15:35 +0700

    modals

--- components/table/BookTable.jsx
+++ components/table/BookTable.jsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import useSWR from "swr";
 import { getBook } from "../../pages/api/book";
 import * as Button from "../button/Button";
+import BookModal from "../modal/BookModal";
 
 const thClass =
   "px-6 align-middle border- border-solid py-3 text-xs border-1-0 border-r-0 whitespace-no-wrap font-semibold text-left bg-gray-100 text-gray-600 border-gray-200";
@@ -52,40 +52,47 @@ const DataBook = () => {
 
 const BookTable = () => {
   return (
-    <div
-      className={
-        "p-4 mx-auto relative flex flex-col min-w-0 break-words w-11/12 mb-6 shadow-lg rounded-lg bg-white"
-      }
-    >
-      <div className="rounded-t mb-0 px-4 py-3 border-0">
-        <div className="flex flex-wrap items-center">
-          <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-            <h1 className="font-semibold text-lg text-gray-800">Book</h1>
+    <>
+      <div
+        className={
+          "p-4 mx-auto relative flex flex-col min-w-0 break-words w-11/12 mb-6 shadow-lg rounded-lg bg-white"
+        }
+      >
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h1 className="font-semibold text-lg text-gray-800">Book</h1>
+            </div>
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <Button.danger
+                className="float-right"
+                onClick={(e) => setOpen(true)}
+              >
+                Add
+              </Button.danger>
+            </div>
           </div>
-          <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-            <Link href="/card">
-              <Button.danger className="float-right">Add</Button.danger>
-            </Link>
-          </div>
-        </div>
 
-        <div className="block-w-full overtime-x-auto mt-4">
-          <table className="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th className={thClass + ` uppercase`}>Number</th>
-                <th className={thClass + ` uppercase`}>Name</th>
-                <th className={thClass + ` uppercase`}>Genre</th>
-                <th className={thClass + ` uppercase`}></th>
-              </tr>
-            </thead>
-            <tbody>
-              <DataBook />
-            </tbody>
-          </table>
+          <div className="block-w-full overtime-x-auto mt-4">
+            <table className="items-center w-full bg-transparent border-collapse">
+              <thead>
+                <tr>
+                  <th className={thClass + ` uppercase`}>Number</th>
+                  <th className={thClass + ` uppercase`}>Name</th>
+                  <th className={thClass + ` uppercase`}>Genre</th>
+                  <th className={thClass + ` uppercase`}></th>
+                </tr>
+              </thead>
+              <tbody>
+                <DataBook />
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+
+      <BookModal />
+    </>
   );
 };
 
